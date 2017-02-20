4a56d1520a2978b240fba3a6789ee3c924461cca
Sam Zipper <samuelczipper@gmail.com>
2017-02-20 11:44:14 -0500

    Update NARR meteo data to include 2014-2015

--- ProcessingScripts/geotop_input_MakeMeteoSpinUpNARR.R
+++ ProcessingScripts/geotop_input_MakeMeteoSpinUpNARR.R
@@ -15,7 +15,7 @@ require(lubridate)
 require(ncdf4)
 
 # filename to save output
-fname.NARR.meteo.out <- paste0(git.dir, "data/meteo/Raw/NARR_Daily_1979-2013.csv")
+fname.NARR.meteo.out <- paste0(git.dir, "data/meteo/Raw/NARR_Daily_1979-2015.csv")
 fname.out <- paste0(git.dir, "geotop/meteo/meteoNARRdailyWithSpinUp0001.txt")
 
 # number of spin-up years
@@ -25,91 +25,91 @@ yr.n <- 100
 site.lat <- 68.93      # unburned=68.93, moderate=68.95, severe=68.99
 site.lon <- -150.27    # unburned=-150.27, moderate=-150.21, severe=-150.28
 
-# # First, build NARR input dataset for 1979-2013 ---------------------------
-# yr.list <- seq(1979,2013)
-# 
-# # get some metadata that will be needed for all stations and shouldn't change between data products or years
-# NARR.nc <- nc_open(paste0(NARR.dir, "air.2m.1979.nc"))
-# NARR.lat <- ncvar_get(NARR.nc, "lat")    # latitude [degN]
-# NARR.lon <- ncvar_get(NARR.nc, "lon")    # longitude [degE]
-# nc_close(NARR.nc)
-# 
-# # find index of your point based on difference between lat/long
-# # get total lat and lon difference and find index of this point
-# site.diff <- abs(NARR.lat-site.lat)+abs(NARR.lon-site.lon)
-# i.NARR <- which(site.diff==min(site.diff), arr.ind=T)
-# 
-# # make empty data.frame
-# df.out  <- data.frame(year = numeric(0),
-#                       DOY = numeric(0),
-#                       Tmean.C = numeric(0),     # mean air temperature at 2 m [K]
-#                       Tdew.C = numeric(0),      # mean dewpoint temperature at 2 m [K]
-#                       precip.mm = numeric(0),   # total precipitation [mm]
-#                       SWin = numeric(0),        # mean downward shortwave radiation [W/m2]
-#                       SWout = numeric(0),       # mean upward shortwave radiation [W/m2]
-#                       LWin = numeric(0),        # mean downward longwave radiation [W/m2]
-#                       LWout = numeric(0),       # mean upward longwave radiation [W/m2]
-#                       P = numeric(0),           # mean air pressure [Pa]
-#                       RH = numeric(0),          # mean relative humidity at 2 m [%]
-#                       wind.u = numeric(0),      # mean u-component of wind at 10 m [m/s]
-#                       wind.v = numeric(0))      # mean v-component of wind at 10 m [m/s]
-# 
-# # scroll through years and grab data
-# for (yr in yr.list){
-#   
-#   # determine number of days in year
-#   yr.days <- yday(ymd(paste0(yr, "-12-31")))
-#   
-#   # open NetCDFs for this year
-#   air.2m.nc <- nc_open(paste0(NARR.dir, "air.2m.", yr, ".nc"))
-#   apcp.nc <- nc_open(paste0(NARR.dir, "apcp.", yr, ".nc"))
-#   dlwrf.nc <- nc_open(paste0(NARR.dir, "dlwrf.", yr, ".nc"))
-#   ulwrf.sfc.nc <- nc_open(paste0(NARR.dir, "ulwrf.sfc.", yr, ".nc"))
-#   dswrf.nc <- nc_open(paste0(NARR.dir, "dswrf.", yr, ".nc"))
-#   uswrf.sfc.nc <- nc_open(paste0(NARR.dir, "uswrf.sfc.", yr, ".nc"))
-#   dpt.2m.nc <- nc_open(paste0(NARR.dir, "dpt.2m.", yr, ".nc"))
-#   pres.sfc.nc <- nc_open(paste0(NARR.dir, "pres.sfc.", yr, ".nc"))
-#   rhum.2m.nc <- nc_open(paste0(NARR.dir, "rhum.2m.", yr, ".nc"))
-#   uwnd.10m.nc <- nc_open(paste0(NARR.dir, "uwnd.10m.", yr, ".nc"))
-#   vwnd.10m.nc <- nc_open(paste0(NARR.dir, "vwnd.10m.", yr, ".nc"))
-#   
-#   # grab data for this year
-#   df.year <- data.frame(year = yr,
-#                         DOY = seq(1,yr.days),
-#                         Tmean.C = ncvar_get(air.2m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days))-273.15,
-#                         Tdew.C = ncvar_get(dpt.2m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days))-273.15,
-#                         precip.mm = ncvar_get(apcp.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         SWin = ncvar_get(dswrf.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         SWout = ncvar_get(uswrf.sfc.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         LWin = ncvar_get(dlwrf.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         LWout = ncvar_get(ulwrf.sfc.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         P = ncvar_get(pres.sfc.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         RH = ncvar_get(rhum.2m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         wind.u = ncvar_get(uwnd.10m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
-#                         wind.v = ncvar_get(vwnd.10m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)))
-#   
-#   # bind to overall data frame
-#   df.out <- rbind(df.year, df.out)
-#   
-#   # close NetCDFs
-#   nc_close(air.2m.nc)
-#   nc_close(apcp.nc)
-#   nc_close(dlwrf.nc)
-#   nc_close(ulwrf.sfc.nc)
-#   nc_close(dswrf.nc)
-#   nc_close(uswrf.sfc.nc)
-#   nc_close(dpt.2m.nc)
-#   nc_close(pres.sfc.nc)
-#   nc_close(rhum.2m.nc)
-#   nc_close(uwnd.10m.nc)
-#   nc_close(vwnd.10m.nc)
-#   
-#   # status update
-#   print(paste0(yr, " complete"))
-# }
-# 
-# # save as CSV
-# write.csv(df.out, fname.NARR.meteo.out, row.names=F, quote=F)
+# First, build NARR input dataset for 1979-2015 ---------------------------
+yr.list <- seq(1979,2013)
+
+# get some metadata that will be needed for all stations and shouldn't change between data products or years
+NARR.nc <- nc_open(paste0(NARR.dir, "air.2m.1979.nc"))
+NARR.lat <- ncvar_get(NARR.nc, "lat")    # latitude [degN]
+NARR.lon <- ncvar_get(NARR.nc, "lon")    # longitude [degE]
+nc_close(NARR.nc)
+
+# find index of your point based on difference between lat/long
+# get total lat and lon difference and find index of this point
+site.diff <- abs(NARR.lat-site.lat)+abs(NARR.lon-site.lon)
+i.NARR <- which(site.diff==min(site.diff), arr.ind=T)
+
+# make empty data.frame
+df.out  <- data.frame(year = numeric(0),
+                      DOY = numeric(0),
+                      Tmean.C = numeric(0),     # mean air temperature at 2 m [K]
+                      Tdew.C = numeric(0),      # mean dewpoint temperature at 2 m [K]
+                      precip.mm = numeric(0),   # total precipitation [mm]
+                      SWin = numeric(0),        # mean downward shortwave radiation [W/m2]
+                      SWout = numeric(0),       # mean upward shortwave radiation [W/m2]
+                      LWin = numeric(0),        # mean downward longwave radiation [W/m2]
+                      LWout = numeric(0),       # mean upward longwave radiation [W/m2]
+                      P = numeric(0),           # mean air pressure [Pa]
+                      RH = numeric(0),          # mean relative humidity at 2 m [%]
+                      wind.u = numeric(0),      # mean u-component of wind at 10 m [m/s]
+                      wind.v = numeric(0))      # mean v-component of wind at 10 m [m/s]
+
+# scroll through years and grab data
+for (yr in yr.list){
+
+  # determine number of days in year
+  yr.days <- yday(ymd(paste0(yr, "-12-31")))
+
+  # open NetCDFs for this year
+  air.2m.nc <- nc_open(paste0(NARR.dir, "air.2m.", yr, ".nc"))
+  apcp.nc <- nc_open(paste0(NARR.dir, "apcp.", yr, ".nc"))
+  dlwrf.nc <- nc_open(paste0(NARR.dir, "dlwrf.", yr, ".nc"))
+  ulwrf.sfc.nc <- nc_open(paste0(NARR.dir, "ulwrf.sfc.", yr, ".nc"))
+  dswrf.nc <- nc_open(paste0(NARR.dir, "dswrf.", yr, ".nc"))
+  uswrf.sfc.nc <- nc_open(paste0(NARR.dir, "uswrf.sfc.", yr, ".nc"))
+  dpt.2m.nc <- nc_open(paste0(NARR.dir, "dpt.2m.", yr, ".nc"))
+  pres.sfc.nc <- nc_open(paste0(NARR.dir, "pres.sfc.", yr, ".nc"))
+  rhum.2m.nc <- nc_open(paste0(NARR.dir, "rhum.2m.", yr, ".nc"))
+  uwnd.10m.nc <- nc_open(paste0(NARR.dir, "uwnd.10m.", yr, ".nc"))
+  vwnd.10m.nc <- nc_open(paste0(NARR.dir, "vwnd.10m.", yr, ".nc"))
+
+  # grab data for this year
+  df.year <- data.frame(year = yr,
+                        DOY = seq(1,yr.days),
+                        Tmean.C = ncvar_get(air.2m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days))-273.15,
+                        Tdew.C = ncvar_get(dpt.2m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days))-273.15,
+                        precip.mm = ncvar_get(apcp.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        SWin = ncvar_get(dswrf.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        SWout = ncvar_get(uswrf.sfc.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        LWin = ncvar_get(dlwrf.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        LWout = ncvar_get(ulwrf.sfc.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        P = ncvar_get(pres.sfc.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        RH = ncvar_get(rhum.2m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        wind.u = ncvar_get(uwnd.10m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)),
+                        wind.v = ncvar_get(vwnd.10m.nc, start=c(i.NARR, 1), count=c(1, 1, yr.days)))
+
+  # bind to overall data frame
+  df.out <- rbind(df.year, df.out)
+
+  # close NetCDFs
+  nc_close(air.2m.nc)
+  nc_close(apcp.nc)
+  nc_close(dlwrf.nc)
+  nc_close(ulwrf.sfc.nc)
+  nc_close(dswrf.nc)
+  nc_close(uswrf.sfc.nc)
+  nc_close(dpt.2m.nc)
+  nc_close(pres.sfc.nc)
+  nc_close(rhum.2m.nc)
+  nc_close(uwnd.10m.nc)
+  nc_close(vwnd.10m.nc)
+
+  # status update
+  print(paste0(yr, " complete"))
+}
+
+# save as CSV
+write.csv(df.out, fname.NARR.meteo.out, row.names=F, quote=F)
 
 # Build data file formatted to GEOtop standards with spin-up --------------
 
