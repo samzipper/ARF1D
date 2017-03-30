7b0f1d8ed2d03fda3f6c5b187c0fc1049f6c84f6
Sam Zipper <samuelczipper@gmail.com>
2017-03-30 17:34:56 -0400

    Fix bug in automated soil property run script

--- geotop_NRCS/DockerAutomateSoilPropertyCalibration.sh
+++ geotop_NRCS/DockerAutomateSoilPropertyCalibration.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for i in {1..100}
-		do
+	do
 		# define the prefix
 		num=`printf %04d $i`
 
 		# copy the appropriate geotop input file, which is created with R
-		cp $(pwd)/SoilPropertyCalibration/${num}_SoilNRCS001.txt $(pwd)/soil/SoilNRCS0001.txt
+		cp $(pwd)/SoilPropertyCalibration/${num}_SoilNRCS0001.txt $(pwd)/soil/SoilNRCS0001.txt
 
 		# run geotop
 		docker run --rm -v $(pwd):/work omslab/geotop
