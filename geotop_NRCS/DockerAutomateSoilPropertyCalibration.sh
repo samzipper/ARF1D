0b3429b0b32823a10ed2d5dd925569b87c2be60a
Sam Zipper <samuelczipper@gmail.com>
2017-04-15 22:48:18 -0500

    Set up for basic 100-sample LHS soil property calibration
    Only calibrating 4 variables:
    -Organic layer Ks
    -Organic layer n
    -Organic layer alpha
    -Organic layer thermal conductivity

---
+++ geotop_NRCS/DockerAutomateSoilPropertyCalibration.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in {1..100}
+	do
+		# define the prefix
+		num=`printf %04d $i`
+
+		# copy the appropriate geotop input file, which is created with R
+		cp $(pwd)/SoilPropertyCalibration/${num}_SoilNRCS0001.txt $(pwd)/soil/SoilNRCS0001.txt
+
+		# run geotop
+		docker run --rm -v $(pwd):/work omslab/geotop
+
+		# copy output file
+		cp $(pwd)/output-tabs/soiltemp0001.txt $(pwd)/SoilPropertyCalibration/output_${num}_soiltemp0001.txt
+		cp $(pwd)/output-tabs/thetaliq0001.txt $(pwd)/SoilPropertyCalibration/output_${num}_thetaliq0001.txt
+
+		# status update
+		echo "$num" complete
+	done
